0ed2238de844f25acaf6fef859f7b7ecf9e27de3
Nariman <abonent26pg@mail.ru>
2023-01-09 01:55:54 +0300

    add hooks and buttn loading more

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import "./portfolio.scss"
 import firstPortfolio from "../../img/portfolio/p1.png";
 import secondPortfolio from "../../img/portfolio/p2.png";
 import ThreePortfolio from "../../img/portfolio/p3.png";
+import { useEffect, useState } from "react";
 
 const portfolioData = [
     {
@@ -17,30 +18,56 @@ const portfolioData = [
         img: ThreePortfolio,
         link: "https://github.com/",
     },
+    {
+        img: firstPortfolio,
+        link: "https://github.com/",
+    },
+    {
+        img: secondPortfolio,
+        link: "https://github.com/",
+    },
+    {
+        img: ThreePortfolio,
+        link: "https://github.com/",
+    },
 ]
 
-const portfolioView = () => {
-    const portfolioCard = portfolioData.map((card, i) => {
-        return (
-            <div className="portfolio_card">
-                <div>
-                    <img className="portfolio__img" src={card.img} alt={card.img} />
-                </div>
-                <a className="portfolio__link" href={card.link} rel="noreferrer" target="_blank">Open this project</a>
-            </div>
-        )
-    })
 
-    return portfolioCard
-}
+
 const Portfolio = () => {
+
+    const [cards, setCard] = useState([])
+    const [count, setCount] = useState(2)
+    const cardCreate = () => {
+        // eslint-disable-next-line
+        const spawn = cards.map((card, id) => {
+            if (id <= count) {
+                return (
+                    <div className="portfolio_card" key={id}>
+                        <div>
+                            <img className="portfolio__img" src={card.img} alt={card.img} />
+                        </div>
+                        <a className="portfolio__link" href={card.link} rel="noreferrer" target="_blank" key={id}>Open this project</a>
+                    </div>
+                )
+            }
+        })
+
+        return spawn
+    }
+
+
+
+    useEffect(() => {
+        setCard(portfolioData)
+    }, [count])
     return (
         <div className="portfolio">
             <div className="container">
                 <h2 className="portfolio__title">Portfolio</h2>
                 <div className="portfolio__inner">
-                    {portfolioView()}
-                    <button className="portfolio__btn">Loading more...</button>
+                    {cardCreate()}
+                    <button className="portfolio__btn" onClick={() => setCount(count => count + 2)}>Loading more...</button>
                 </div>
             </div>
         </div>
